a7457440ecd2566186906cf282765072aeb2d563
Tudor Borca <80685744+Tudor036@users.noreply.github.com>
2022-11-27 06:54:27 +0200

    finished business endpoints

--- src/controllers/businesses.ts
+++ src/controllers/businesses.ts
@@ -1,24 +1,87 @@
 import { Request, Response } from "express";
+import { DataProperties } from "../models/Business.model";
+import { createOneBusiness, deleteOneBusiness, fetchOneBusiness, updateOneBusiness } from "../service/business.service";
+import { fetchBusinesses } from "../utils/firebase.utils";
 
-export const getBusinesses = (req: Request, res: Response) => {
-    res.send('Sent a list of businesses');
+export const getBusinesses = async (req: Request, res: Response) => {
+    let businesses;
+    try {
+        businesses = await fetchBusinesses();
+    } catch(err: any) {
+        console.log(err);
+        res.send(500).json({
+            status: "failed",
+            message: "Internal Server Error"
+        })
+    }
+    res.send(businesses);
 }
 
-export const getBusiness = (req: Request, res: Response) => {
+export const getBusiness = async (req: Request, res: Response) => {
     const businessId = req.params.businessId;
-    res.send(`Sent business with id ${businessId}`);
+    let searchedBusiness;
+    try {
+        searchedBusiness = await fetchOneBusiness(businessId);
+    } catch(err: any) {
+        console.error(err);
+        res.status(500).json({
+            status: "failed",
+            message: "Internal Server Error"
+        })
+    }
+    res.status(200).json(searchedBusiness);
 }
 
-export const createBusiness = (req: Request, res: Response) => {
-    res.send('Posted New Business');
+export const createBusiness = async (req: Request, res: Response) => {
+    const data: DataProperties = req.body;
+    let newBusiness;
+    try {
+        newBusiness = await createOneBusiness(data);
+    } catch(err) {
+        console.error(err);
+        res.status(500).json({
+            status: "failed",
+            message: "Internal Server Error"
+        })
+    }
+    res.status(201).json({
+        status: "success",
+        message: "Document updated succesfully!"
+    });
 }
 
-export const updateBusiness = (req: Request, res: Response) => {
+export const updateBusiness = async (req: Request, res: Response) => {
     const businessId = req.params.businessId;
-    res.send(`Updated business with id ${businessId}`);
+    const newData = req.body;
+    try {
+        await updateOneBusiness(businessId, newData);
+    } catch(err: any) {
+        console.log(err);
+        res.status(400).json({
+            status: "failed",
+            message: "Bad Request"
+        })
+    }
+    res.status(204).json({
+        status: "success",
+        message: "Document updated succesfully!"
+    });
 };
 
-export const deleteBusiness = (req: Request, res: Response) => {
+export const deleteBusiness = async (req: Request, res: Response) => {
     const businessId = req.params.businessId;
-    res.send(`Delete business with id ${businessId}`);
+    try {
+        await deleteOneBusiness(businessId);
+    } catch(err) {
+        console.error(err);
+        res.status(500).json({
+            status: "failed",
+            message: "Bad Request"
+        });
+    }
+
+    res.status(210).json({
+        status: "success",
+        message: "Document deleted succesfully!"
+    });;
 };
